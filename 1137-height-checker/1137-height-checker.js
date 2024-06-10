9135b5928a74d9deccd8d3b56ab6ca7666459122
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-06-10 09:55:16 +0530

    Time: 68 ms (12.63%), Space: 52.5 MB (8.59%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function (heights) {
+    let count = 0
+    let a=[...heights]
+heights.sort((a,b)=>a-b)
+for(let i=0;i<heights.length;i++){
+    if(a[i]!=heights[i]){
+        count++
+    }
+}
+
+    console.log(heights)
+    console.log(a)
+        return count
+    
+
+};

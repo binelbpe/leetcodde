17e2dd572447b98556edaa15e4a967c3c57cab7f
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-06-12 09:36:33 +0530

    Time: 64 ms (8.58%), Space: 49.1 MB (28.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    let temp
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
+        }
+    }
+    return nums
+};

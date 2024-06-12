a16670e3a093e95f1928fc6e0f2eebb3801ea513
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-06-12 10:54:52 +0530

    Time: 43 ms (90.99%), Space: 48.7 MB (57.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+   
+    let dp = [0,1,2]
+
+    let currentStep = 3
+
+    while(currentStep <= n){
+        dp[currentStep] = dp[currentStep-1] + dp[currentStep - 2]
+        currentStep++
+    }
+
+    return dp[n] 
+};

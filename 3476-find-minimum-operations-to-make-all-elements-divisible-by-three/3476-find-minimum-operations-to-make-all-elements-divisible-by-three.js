6e1721c6bf0a2bbd01f67f24a801de5e7be29ca5
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-06-29 09:17:54 +0530

    Time: 56 ms (72.97%), Space: 49.9 MB (59.54%) - LeetHub

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/3476-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let count=0;
+    let len=nums.length;
+  for(let i=0;i<len;i++){
+    if(nums[i]%3!=0) count++;
+    }
+return count;
+};

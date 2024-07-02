740812eafeeb98894c8b53f1dc7f00be70b303f6
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-07-02 13:03:04 +0530

    Time: 77 ms (20.02%), Space: 52.5 MB (38.97%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -2,16 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let flag = false
-        for (let j = 0; j < nums.length; j++) {
-            if (i != j && nums[i] == nums[j]) {
-                flag = true
-            }
-        }
-        if (!flag) {
-            return nums[i]
-        }
+var singleNumber = function(nums) {
+    nums.sort((a,b)=>a-b)
+  for(let i=0;i<nums.length;i++){
+    if(nums[i]==nums[i+1]){
+        i=i+1
+    }else{
+        return nums[i]
     }
+  }
 };

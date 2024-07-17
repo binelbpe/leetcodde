c46644e466de3488ec39d123ee65317497300327
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-07-17 09:36:11 +0530

    Time: 55 ms (48.78%), Space: 51.1 MB (7.73%) - LeetHub

--- 2805-array-wrapper/2805-array-wrapper.js
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -2,38 +2,20 @@
  * @param {number[]} nums
  * @return {void}
  */
-var ArrayWrapper = function (nums) {
-    this.nums = nums
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
 };
 
 /**
  * @return {number}
  */
-ArrayWrapper.prototype.valueOf = function () {
-
-    if (this.nums.length == 0) {
-        return 0
-    } else {
-        let sum = 0;
-        for (let i of this.nums) {
-            sum += i
-        }
-        return sum
-    }
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((acc, val) => acc + val, 0);
 }
 
 /**
  * @return {string}
  */
-ArrayWrapper.prototype.toString = function () {
-    return JSON.stringify(this.nums)
-
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(',')}]`;
 }
-
-/**
- * const obj1 = new ArrayWrapper([1,2]);
- * const obj2 = new ArrayWrapper([3,4]);
- * obj1 + obj2; // 10
- * String(obj1); // "[1,2]"
- * String(obj2); // "[3,4]"
- */

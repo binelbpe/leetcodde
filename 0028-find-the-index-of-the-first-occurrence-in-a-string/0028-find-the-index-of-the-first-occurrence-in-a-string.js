2fc4bb3808bad1c4476fac10f9987caae73ea07c
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-07-31 15:18:59 +0530

    Time: 61 ms (16.82%), Space: 48.9 MB (52.77%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let x =haystack.indexOf(needle)
+    return x
+};

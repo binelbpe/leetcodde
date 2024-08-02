beb608dd060aa1b0b7da92a7c387220650371d26
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-08-02 09:19:39 +0530

    Time: 54 ms (74.86%), Space: 51.1 MB (43.71%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.js
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.js
@@ -2,22 +2,18 @@
  * @param {number[]} arr
  * @return {number}
  */
-var findSpecialInteger = function(arr) {
-    let a=[]
-    let b=[]
-    count=1
-        let c=arr.length
-    for(let i=0;i<c;i++){
-        if(arr[i]!=arr[i+1]){
-             if((count/c*100)>25){
-            return arr[i]
-        }
-a.push(count)
-b.push(arr[i])
-count=0
-console.log(a,b)
+var findSpecialInteger = function (arr) {
+
+    count = 1
+    let c = arr.length
+    for (let i = 0; i < c; i++) {
+        if (arr[i] != arr[i + 1]) {
+            if ((count / c * 100) > 25) {
+                return arr[i]
+            }
+            count = 0
         }
         count++
     }
-   
+
 };

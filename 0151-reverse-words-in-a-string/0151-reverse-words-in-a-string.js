72ddf5062a7ae4b5d829443b56a930bc8f2088dc
binelbpe <156921025+binelbpe@users.noreply.github.com>
2024-08-10 09:16:44 +0530

    Time: 62 ms (24.64%), Space: 50.4 MB (84.41%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -2,14 +2,8 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    let arr=[]
-   for(let i=0;i<s.length;i++){
-    if(s[i]==" "&&s[i+1]==" "){
-        continue
-    }
-arr.push(s[i])
-   }
-    return arr.join("").split(" ").reverse().join(" ").trim()
-    
+var reverseWords = function (s) {
+
+    return s.trim().split(/\s+/).reverse().join(" ")
+
 };
